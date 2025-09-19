1ac19126701c373e8ce79ccd8c99b3e46b8e73c6
FelipeLima <felipenascimentolima1@gmail.com>
2025-09-18 22:22:37 -0300

    conectando o document_loader com ChatGoogleGenerativeAI

---
+++ utils/document_loader.py
@@ -0,0 +1,50 @@
+from config_key import GOOGLE_API_KEY
+from screening import llm_triagem
+
+from pathlib import Path
+
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+# pymupdf - para ler os PDF
+docs =[]
+
+
+for n in Path("./docs/").glob("*.pdf"):
+  try:
+    loader = PyMuPDFLoader(str(n))
+    docs.extend(loader.load())
+    print(f"Carregado com sucesso: {n.name}")
+  except Exception as e:
+    print(f"Erro ao carregar: {n.name}: {e}")
+
+
+# langchain-text-splitters - para quebrar o texto em pequenos pedaços
+splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=30)
+chunks = splitter.split_documents(docs)
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/gemini-embedding-001",
+    google_api_key=GOOGLE_API_KEY
+)
+
+vectorstore = FAISS.from_documents(chunks, embeddings)
+
+retriever = vectorstore.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={"score_threshold": 0.3, "k": 4}
+)
+
+prompt_rag = ChatPromptTemplate.from_messages([
+    ("system",
+     "Você é um Assistente de Políticas Internas (RH/IT) da empresa Carraro Desenvolvimento. "
+     "Responda SOMENTE com base no contexto fornecido. "
+     "Se não houver base suficiente, responda apenas 'Não sei'."),
+
+    ("human", "Pergunta: {input}\n\nContexto:\n{context}")
+])
+
+document_chain = create_stuff_documents_chain(llm_triagem, prompt_rag)
